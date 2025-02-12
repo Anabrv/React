ab4d747e167d7392f175c31a74c534dc17a21ac2
Anabrv <anabrvigil@gmail.com>
2025-02-12 23:32:42 +0100

    mejoraso

--- ejercicioFotos/src/App.jsx
+++ ejercicioFotos/src/App.jsx
@@ -6,52 +6,48 @@ import React from 'react'
 import BotonSaludar from './componentes/BotonSaludar'
 
 function App() {
- 
-    const [name,setName]= useState("");
-    const [edad,setEdad]= useState("");
-    const [mostrar,setMostrar]= useState("");
-    
-    const handleName = (e)=>{
-      setName(e.target.value);
-      
-    }
-    const handleEdad = (e) => {
-      setEdad(e.target.value);
-    }
-    /* MOSTRAR POR BOTON */
-    const mostrarInfo = () =>{
-      console.log("cliky")
-      setMostrar(compararEdad(edad));
-    }
 
-    const compararEdad = (edad) => {
-      if(edad < 12 ){
-        setMostrar(`Hola, ${name}, eres un nene`);
-      } else if(edad > 12 && edad < 20){
-        setMostrar(`Hola, ${name}, eres joven`);
-      } else if(edad > 20 && edad < 40){
-        setMostrar(`Hola, ${name}, eres mayor`);
-      }
-      else{
-        setMostrar(`Hola, ${name}, eres un anciano`);
-      }
-    }
+  const [name, setName] = useState("");
+  const [edad, setEdad] = useState("");
+  const [mostrar, setMostrar] = useState("");
 
-    
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEdad = (e) => {
+    setEdad(e.target.value);
+  }
+
+  const mostrarInfo = () => {
+    console.log("click");
+    compararEdad(edad);
+  }
+
+  const compararEdad = (edad) => {
+    if (edad < 12) {
+      setMostrar(`Hola, ${name}, eres un nene`);
+    } else if (edad >= 12 && edad < 20) {
+      setMostrar(`Hola, ${name}, eres joven`);
+    } else if (edad >= 20 && edad < 40) {
+      setMostrar(`Hola, ${name}, eres mayor`);
+    } else {
+      setMostrar(`Hola, ${name}, eres un anciano`);
+    }
+  }
+
   return (
     <div className='container'>
-    <h1>Nombre: </h1>
-    <input onChange={handleName} value={name} type="text" />
-    <h1>Edad: </h1>
-    <input onChange={handleEdad} value={edad} type="number" />
-    <br />
-     <BotonSaludar onClick={mostrarInfo}/> 
+      <h1>Nombre: </h1>
+      <input onChange={handleName} value={name} type="text" />
+      <h1>Edad: </h1>
+      <input onChange={handleEdad} value={edad} type="number" />
+      <br />
+      <BotonSaludar onClick={mostrarInfo}/> 
 
-{/* SI NO PONGO LA VARIABLE, NO SE MUESTRA */}
-    <h1>{mostrar}</h1>
+      <h1>{mostrar}</h1>
     </div>
   )
 }
+
 export default App
-
-
